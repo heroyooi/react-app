64ce3eee7d47ae407a2f1ff5b7178633df8b71e5
heroyooi <heroyooi1018@gmail.com>
2025-07-11 15:43:25 +0900

    1-3. JSX 기초와 컴포넌트 구조 - 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import './App.css';
+import UserCard from './components/UserCard';
 
 function App() {
-  return <h1>Hello, React!</h1>;
+  return (
+    <div>
+      <h1>👋 React Props 예제</h1>
+      <UserCard name="홍길동" age={30} />
+      <UserCard name="김철수" age={24} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,9 @@
+function UserCard({ name, age }) {
+  return (
+    <div>
+      <p>{name}님은 {age}세입니다.</p>
+    </div>
+  );
+}
+
+export default UserCard;
