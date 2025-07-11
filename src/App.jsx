210a43577bf9a18975f3e5ddc629537b9261ee0e
heroyooi <heroyooi1018@gmail.com>
2025-07-11 16:09:57 +0900

    2-1. Props와 State의 차이점과 사용법 - 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css';
+import LikeButton from './components/LikeButton';
 
 function App() {
-  return <h1>Hello, React!</h1>;
+  return (
+    <div>
+      <h1>React Like Button 예제</h1>
+      <LikeButton />
+    </div>
+  );
 }
 
 export default App;
