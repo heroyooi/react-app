8869cedbef17bece747ddb86db02ae3e617e58ff
heroyooi <heroyooi1018@gmail.com>
2025-07-15 12:15:51 +0900

    4-3. 중첩 라우팅과 레이아웃 적용 - 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardLayout from './components/DashboardLayout';
+import DashboardHome from './pages/DashboardHome';
+import DashboardSettings from './pages/DashboardSettings';
 
 function App() {
-  return <h1>Hello, React!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard' element={<DashboardLayout />}>
+          <Route index element={<DashboardHome />} />
+          <Route path='settings' element={<DashboardSettings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/DashboardHome.jsx
@@ -0,0 +1,5 @@
+function DashboardHome() {
+  return <h2>🏠 대시보드 홈입니다</h2>;
+}
+
+export default DashboardHome;
